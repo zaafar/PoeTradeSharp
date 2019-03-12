a226f9e976e57642d16406d8760fe52a7ebe210a
Zaafar Ahmed <zaafar.tahir@gmail.com>
2019-03-11 22:36:20 -0400

    added code to convert Property type to fields to send to the server for sorting

---
+++ PoeTradeSharp/Helpers.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="Helpers.cs" company="Zaafar Ahmed">
+//     Zaafar
+// </copyright>
+
+namespace PoeTradeSharp
+{
+    /// <summary>
+    /// A bunch of helper functions extracted from the pathofexile JS code
+    /// </summary>
+    public static class Helpers
+    {
+        /// <summary>
+        /// This function converts the result -> 0 -> item -> properties ---select property-> type value
+        /// to the Field that should be send to the server for sorting asc/dec.
+        /// </summary>
+        private static string[] propertyTypeToFieldName = new string[]
+        {
+            string.Empty,
+            "map_tier",
+            "map_iiq",
+            "map_iir",
+            "map_packsize",
+            "gem_level",
+            "quality",
+            string.Empty,
+            string.Empty,
+            "pdamage",
+            "edamage",
+            "cdamage",
+            "crit",
+            "aps",
+            string.Empty,
+            "block",
+            "ar",
+            "ev",
+            "es",
+            string.Empty,
+            "gem_level_progress",
+            string.Empty,
+            string.Empty,
+            string.Empty
+        };
+
+        /// <summary>
+        /// This function converts the result -> 0 -> item -> properties ---select property-> type value
+        /// to the Field that should be send to the server for sorting asc/dec.
+        /// </summary>
+        public static string[] PropertyTypeToFieldName => propertyTypeToFieldName;
+    }
+}
